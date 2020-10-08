23e0a418ccf378cae2f476d737b3a4ca5be16be6
Antoine GAUTIER <antoine.gautier@lft.ca>
2020-10-08 15:07:44 -0400

    recover from GaProject

--- gaplugin-form-plugin.php
+++ gaplugin-form-plugin.php
@@ -1,12 +1,59 @@
 <?php
+<?php
 /*
-Plugin Name: Example Contact Form Plugin
-Plugin URI: http://example.com
-Description: Simple non-bloated WordPress Contact Form
-Version: 1.0
-Author: Agbonghama Collins
-Author URI: http://w3guy.com
+Plugin Name: Form-GA
+Plugin URI: https://github.com/Pepite61/gaplugin-form
+Description: Form for the website
+Version: 0.00.01
+Requires at least: 5.2
+Requires PHP: 7.2
+Author: GAUTIER Antoine
+Author URI: gautierantoine.com
+Text Domain: form-text
+Domain Path: /languages
+License:     GPL v3
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+Form-GA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+Form-GA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with DarkMode-GA.
+If not, see https://www.gnu.org/licenses/gpl-3.0.en.html.
 */
+  //
+  // defined('ABSPATH') or die('You cannot enter');
+  // add_filter(
+  //   'rest_authentication_errors',
+  //   function( $result ) {
+  //     if ( true === $result || is_wp_error( $result ) ) {
+  //         return $result;
+  //     }
+  //     if ( ! is_user_logged_in() ) {
+  //         return new WP_Error(
+  //             'rest_not_logged_in',
+  //             __( 'You are not currently logged in', 'form-text' ),
+  //             array( 'status' => 401 )
+  //         );
+  //     }
+  //     return $result;
+  //   }
+  // );
+  // if (!class_exists('GAPlugin\AdminPage')){
+  //   require_once 'includes/AdminPage.php';
+  // }
+  // require_once 'includes/Form.php';
+  //
+  //
+  // register_uninstall_hook( __FILE__, ['GAPlugin\Form', 'removeOptions']);
+  //
+  // add_action(
+  //   'init',
+  //   function () {
+  //     GAPlugin\Form::register();
+  //   }
+  // );
+
+
+
 
 function html_form_code() {
 
